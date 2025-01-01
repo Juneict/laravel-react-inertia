a013c60be3fb484a209094112144c84cb4e60035
Juneict <junemoenyinyiict@gmail.com>
2025-01-01 22:22:42 +0700

    add:posts

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('post', PostController::class)->except('index');
