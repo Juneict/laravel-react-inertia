34e24e943ffe5e17a0ba28ffb7151ed35efd6369
Juneict <junemoenyinyiict@gmail.com>
2025-01-02 09:32:42 +0700

    add:paginate

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,19 +1,44 @@
-export default function Home({posts}) {
+import { Link } from "@inertiajs/react";
+
+export default function Home({ posts }) {
     console.log(posts);
     return (
         <>
             <h1 className="title">Hello</h1>
             <div>
-                {posts.map(post => (
-                    <div key={post.key} className="p-4 border-b">
+                {posts.data.map((post) => (
+                    <div key={post.id} className="p-4 border-b">
                         <div>
                             <span>Posted on : </span>
-                            <span>{ new Date(post.created_at).toLocaleTimeString()}</span>
+                            <span>
+                                {new Date(post.created_at).toLocaleTimeString()}
+                            </span>
                         </div>
                         <p className="font-medium">{post.body}</p>
                     </div>
                 ))}
             </div>
+            <div className="py-12 px-4">
+                {posts.links.map((link) => (
+                    link.url ? (
+                        <Link
+                            key={link.label}
+                            href={link.url} 
+                            dangerouslySetInnerHTML={{__html : link.label}}
+                            className={`p-1 mx-1 ${link.active ? "text-blue-700 font-bold" : '' }`}
+                        >
+                       </Link>
+                    ) : (
+                        <span
+                            key={link.label}
+                            dangerouslySetInnerHTML={{__html : link.label}}
+                            className="p-1 mx-1 text-slate-500"
+                        >
+                        </span>
+                    )
+                   
+                ))}
+            </div>
         </>
-    )
+    );
 }
